2b52408a7cae43887686f74e3b6377cff5b761fe
Daniel Felix Ferber <danielferber@yahoo.com.br>
2015-06-02 23:17:06 -0300

    [dev]

--- exception-toys/src/main/java/org/usefultoys/exception/Assert.java
+++ exception-toys/src/main/java/org/usefultoys/exception/Assert.java
@@ -1,5 +1,8 @@
 package org.usefultoys.exception;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.usefultoys.exception.datastate.IllegalArgumentDataException;
 import org.usefultoys.exception.datastate.IllegalAttributeDataException;
 import org.usefultoys.exception.datastate.IllegalEnvironmentDataException;
@@ -8,10 +11,8 @@ import org.usefultoys.exception.datastate.IllegalPosConditionException;
 import org.usefultoys.exception.datastate.IllegalPreConditionException;
 
 /**
- * A set of objects that implement many typical validation of assertions.
- * Simplifies raising specialized exceptions in case of assertion errors by
- * providing methods that implement recurring validation patterns.
- * Of course, the respective exceptions may be risen
+ * A collection typical validation methods.
+ * Simplifies raising specialized exceptions for assertion errors.
  *
  * @author Daniel Felix Ferber
  */
@@ -20,13 +21,17 @@ public abstract class Assert {
 		// cannot create instance
 	}
 
-	public static final Assert Argument = new Assert() {
+	public static final Assert Argument = new Argument();
+	
+	public static class Argument extends Assert {
 		@Override protected RuntimeException createException(Object rule) { return new IllegalArgumentDataException(rule); }
 		@Override protected RuntimeException createException(String message) { return new IllegalArgumentDataException(message); }
 		@Override protected RuntimeException createException() { return new IllegalArgumentDataException(); }
 	};
 
-	public static final Assert Attribute = new Assert() {
+	public static final Assert Attribute = new Attribute ();
+	
+	public static class Attribute extends Assert {
 		@Override protected RuntimeException createException(Object rule) { return new IllegalAttributeDataException(rule); }
 		@Override protected RuntimeException createException(String message) { return new IllegalAttributeDataException(message); }
 		@Override protected RuntimeException createException() { return new IllegalAttributeDataException(); }
@@ -43,21 +48,27 @@ public abstract class Assert {
 			if (System.getenv(name) == null) throw createException(String.format("%s must exist in environment", name));
 			return true;
 		}
-	}
+	};
 
-	public static final Assert Precondition = new Assert() {
+	public static final Assert Precondition = new Precondition();
+	
+	public static class Precondition extends Assert {
 		@Override protected RuntimeException createException(Object rule) { return new IllegalPreConditionException(rule); }
 		@Override protected RuntimeException createException(String message) { return new IllegalPreConditionException(message); }
 		@Override protected RuntimeException createException() { return new IllegalPreConditionException(); }
 	};
 
-	public static final Assert Poscondition = new Assert() {
+	public static final Assert Poscondition = new Poscondition();
+
+	public static class Poscondition extends Assert {
 		@Override protected RuntimeException createException(Object rule) { return new IllegalPosConditionException(rule); }
 		@Override protected RuntimeException createException(String message) { return new IllegalPosConditionException(message); }
 		@Override protected RuntimeException createException() { return new IllegalPosConditionException(); }
 	};
 
-	public static final Assert Invariant = new Assert() {
+	public static final Assert Invariant = new Invariant();
+	
+	public static class Invariant extends Assert {
 		@Override protected RuntimeException createException(Object rule) { return new IllegalInvariantException(rule); }
 		@Override protected RuntimeException createException(String message) { return new IllegalInvariantException(message); }
 		@Override protected RuntimeException createException() { return new IllegalInvariantException(); }
@@ -67,54 +78,174 @@ public abstract class Assert {
 	protected abstract RuntimeException createException(String message);
 	protected abstract RuntimeException createException(Object rule);
 
-	/** Argument must satisfy a boolean condition. */
+	// ************************************************************************
+	// *** CUSTOM *************************************************************
+	// ************************************************************************
+	
+	/** A custom condition must be satisfied. */
 	public final boolean check(boolean condition) throws RuntimeException {
 		if (! condition) throw createException();
 		return true;
 	}
 
-	/** Argument(s) must satisfy one or more boolean condition(s). */
-	public final boolean check(boolean ... conditions) throws RuntimeException {
+	/** A custom condition must be satisfied. */
+	public final boolean check(Object rule, boolean expression) throws RuntimeException {
+		if (! expression) throw createException(rule);
+		return true;
+	}
+
+	/** All custom conditions must be satisfied. */
+	public final boolean checkAll(boolean ... conditions) throws RuntimeException {
 		for (boolean b : conditions) {
 			if (!b) throw createException();
 		}
 		return true;
 	}
 
-	/** Argument must satisfy a boolean condition expected by a system rule. */
-	public final boolean check(Object rule, boolean expression) throws RuntimeException {
-		if (! expression) throw createException(rule);
+	/** All custom conditions must be satisfied. */
+	public final boolean checkAll(Object rule, boolean ... conditions) throws RuntimeException {
+		for (boolean b : conditions) {
+			if (!b) throw createException(rule);
+		}
 		return true;
 	}
 
-	/** Argument(s) must be equal to expected value. */
+	// ************************************************************************
+	// *** NON NULL ***********************************************************
+	// ************************************************************************
+
+	/** Argument(s) must not be <code>null</code>. */
+	public final <T> boolean notNull(T argument) throws RuntimeException {
+		if (argument == null) throw createException("null");
+		return true;
+	}
+
+	/** Argument(s) must not be <code>null</code>. */
+	public final boolean notNull(Object ... arguments) throws RuntimeException {
+		for (Object b : arguments) {
+			if (b == null) throw createException("null");
+		}
+		return true;
+	}
+
+	// ************************************************************************
+	// *** EQUAL **************************************************************
+	// ************************************************************************
+
+	/** Given value must be equal to the expected value. */
 	public final boolean equal(int value, int expected) throws RuntimeException {
-		if (value != expected) throw createException(String.format("%d must be equal to %d]", Integer.valueOf(value), Integer.valueOf(expected)));
+		if (value != expected) throw createException(String.format("%d must be equal to %d]", value, expected));
 		return true;
 	}
 
-	/** Argument(s) must be equal to expected value. */
+	/** Given value must be equal to the expected value. */
+	public final boolean equal(long value, long expected) throws RuntimeException {
+		if (value != expected) throw createException(String.format("%d must be equal to %d]", value, expected));
+		return true;
+	}
+
+	/** Given value must be equal to the expected value. */
 	public final <T> boolean equal(T value, T expected) throws RuntimeException {
 		if (value == expected) return true;
 		if (value != expected && (value == null || ! value.equals(expected))) throw createException(String.format("%d must be equal to %d]", Assert.nullToStr(value), Assert.nullToStr(expected)));
 		return true;
 	}
 
-	/** Argument(s) must be within a range. */
+	// ************************************************************************
+	// *** RANGE **************************************************************
+	// ************************************************************************
+
+	/** Value must be within a range. */
 	public final boolean range(int value, int min, int max) throws RuntimeException {
-		if (value < min || value > max ) throw createException(String.format("%d must be in range [%d-%d]", Integer.valueOf(value), Integer.valueOf(min), Integer.valueOf(max)));
+		if (value <= min || value >= max ) throw createException(String.format("%d must be in range [%d-%d]", value, min, max));
 		return true;
 	}
 
-	/** Argument(s) must not be negative ( >= 0). */
-	public final boolean notNegative(int value) throws RuntimeException {
-		if (value < 0) throw createException(String.format("%d must not be negative", Integer.valueOf(value)));
+	/** Value must be within a range. */
+	public final boolean range(long value, long min, long max) throws RuntimeException {
+		if (value <= min || value >= max ) throw createException(String.format("%d must be in range [%d-%d]", value, min, max));
 		return true;
 	}
+
+	/** Value must be within a range. */
+	public final <T extends Comparable<T>> boolean range(T value, T min, T max) throws RuntimeException {
+		if (value == null) throw createException(String.format("value must not be null", value, min, max));
+		if (value.compareTo(min) < 0 || value.compareTo(max) > 0 ) throw createException(String.format("%s must be in range [%s-%s]", value.toString(), min.toString(), max.toString()));
+		return true;
+	}
+
+
+	// ************************************************************************
+	// *** ARRAY **************************************************************
+	// ************************************************************************
+
+	/** Value must be an index of given array. */
+	public final boolean range(int value, int array[]) throws RuntimeException {
+		if (value < 0 || value >= array.length) throw createException(String.format("%d must be in range [0-%d]", value, 0, array.length-1));
+		return true;
+	}
+
+	/** Value must be an index of given array. */
+	public final boolean range(int value, long array[]) throws RuntimeException {
+		if (value < 0 || value >= array.length) throw createException(String.format("%d must be in range [0-%d]", value, 0, array.length-1));
+		return true;
+	}
+
+	/** Value must be an index of given array. */
+	public final boolean range(int value, boolean array[]) throws RuntimeException {
+		if (value < 0 || value >= array.length) throw createException(String.format("%d must be in range [0-%d]", value, 0, array.length-1));
+		return true;
+	}
+
+	/** Value must be an index of given array. */
+	public final boolean range(int value, float array[]) throws RuntimeException {
+		if (value < 0 || value >= array.length) throw createException(String.format("%d must be in range [0-%d]", value, 0, array.length-1));
+		return true;
+	}
+
+	/** Value must be an index of given array. */
+	public final boolean range(int value, double array[]) throws RuntimeException {
+		if (value < 0 || value >= array.length) throw createException(String.format("%d must be in range [0-%d]", value, 0, array.length-1));
+		return true;
+	}
+
+	/** Value must be an index of given array. */
+	public final boolean range(int value, Object array[]) throws RuntimeException {
+		if (value < 0 || value >= array.length) throw createException(String.format("%d must be in range [0-%d]", value, 0, array.length-1));
+		return true;
+	}
+
+	public final boolean range(int value, List<?> list) throws RuntimeException {
+		if (value < 0 || value >= list.size()) throw createException(String.format("%d must be in range [0-%d]", value, 0, list.size()-1));
+		return true;
+	}
+
+	// ************************************************************************
+	// *** CONTAINS ***********************************************************
+	// ************************************************************************
+
+	public final boolean contains(Object value, Collection<?> collection) throws RuntimeException {
+		if (! collection.contains(value)) throw createException(String.format("collection must contain %s", value.toString()));
+		return true;
+	}
+
+	// ************************************************************************
+	// *** NON NEGATIVE *******************************************************
+	// ************************************************************************
+
+	/** Values must be non negative (greater or equal to zero). */
+	public final boolean nonNegative(int value) throws RuntimeException {
+		if (value < 0) throw createException(String.format("%d must not be negative", value));
+		return true;
+	}
+
+	// ************************************************************************
+	// *** POSITIVE ***********************************************************
+	// ************************************************************************
 
 	/** Argument(s) must not be positive ( > 0). */
 	public final boolean positive(int value) throws RuntimeException {
-		if (value <= 0) throw createException(String.format("%d must be positive", Integer.valueOf(value)));
+		if (value <= 0) throw createException(String.format("%d must be positive", value));
 		return true;
 	}
 	public final boolean positive(Integer value) throws RuntimeException {
@@ -130,20 +261,6 @@ public abstract class Assert {
 		return true;
 	}
 
-
-	/** Argument(s) must not be <code>null</code>. */
-	public final <T> boolean notNull(T argument) throws RuntimeException {
-		if (argument == null) throw createException("null");
-		return true;
-	}
-
-	/** Argument(s) must not be <code>null</code>. */
-	public final boolean notNull(Object ... arguments) throws RuntimeException {
-		for (Object b : arguments) {
-			if (b == null) throw createException("null");
-		}
-		return true;
-	}
 //
 //	/** Argument(s) must not be <code>null</code> as expected by a system rule. */
 //	public final <T> boolean notNull(Object rule, T argument) throws RuntimeException {
@@ -152,7 +269,8 @@ public abstract class Assert {
 //	}
 
 	private static Object nullToStr(Object o) {
-		if (o == null) return "null";
+		if (o == null)
+			return "null";
 		return o;
 	}
 }
